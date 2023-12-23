6f6a5603aded1fdbc4b26bdfa78df038b45a0af6
Naveen Kumar <nvnkr09@gmail.com>
2023-12-23 21:00:40 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Profile from "./Components/Profile";
+import SignIn from "./Components/Signin";
 
 function App() {
+  const [userDetails, setUserDetails] = useState({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {localStorage.getItem('user') ? (
+        <Profile userDetails={userDetails} />
+      ) : (
+        <SignIn setUserDetails={setUserDetails} />
+      )}
     </div>
   );
 }
---
+++ src/Components/Profile.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "../Styles/profile.css";
+import ProfileSVG from "../Assets/user-1-svgrepo-com.svg";
+
+const Profile = ({userDetails}) => {
+
+    const handleLogout = () => {
+        // Clear user data from local storage on logout
+        localStorage.removeItem('user');
+        window.location.href = '';
+      };
+
+  return (
+    <div className="main">
+      <div className="design">
+        <div className="line"></div>
+      </div>
+      <h1>Signup Successful!</h1>
+      <div className="form">
+        <h1>Profile</h1>
+        <img src={ProfileSVG} alt="Profile Image" />
+        <div className="details">
+          <p>User ID: {userDetails.id}</p>
+          <p>Full Name: {userDetails.name}</p>
+          <p>Email: {userDetails.username}</p>
+        </div>
+        <button className="logout-button" onClick={handleLogout}>LOGOUT</button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
