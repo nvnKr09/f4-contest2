40ca5aa9959dccd7b8fb668e576711447251b957
Naveen Kumar <nvnkr09@gmail.com>
2023-12-23 21:22:23 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       {localStorage.getItem('user') ? (
-        <Profile userDetails={userDetails} />
+        <Profile userDetails={userDetails} setUserDetails={setUserDetails} />
       ) : (
         <SignIn setUserDetails={setUserDetails} />
       )}
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from "react";
 import "../Styles/profile.css";
 import ProfileSVG from "../Assets/user-1-svgrepo-com.svg";
 
-const Profile = ({userDetails}) => {
+const Profile = ({userDetails,setUserDetails}) => {
+
+    useEffect(() => {
+        // Fetch user details from local storage when the component mounts
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+          setUserDetails(storedUser);
+        }
+      }, []);
 
     const handleLogout = () => {
         // Clear user data from local storage on logout
         localStorage.removeItem('user');
-        window.location.href = '';
+        window.location.href = '/';
       };
 
   return (
